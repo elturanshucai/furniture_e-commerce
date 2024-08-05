95908e2153225f3a5fa55428e8c70bf5b9e8d7f2
elturanshucai <elturanfcb@gmail.com>
2024-08-05 12:19:14 +0400

    footer ui completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import styles from './footer.module.css'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <footer></footer>
+    <footer className={styles.footer}>
+      <section className={styles.top}>
+        <h2 className={styles.title}>HomeDecor</h2>
+        <article className={styles.info}>
+          <div>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          </div>
+          <Link to={"tel:+994 50 555 55 55"}>+994 50 555 55 55</Link>
+          <Link to={"mailto:youremailhere@gmail.com"}>youremailhere@gmail.com</Link>
+        </article>
+      </section>
+      <p className={styles.p}>
+        ©2022 All Right Reserved. Developed by Webcoder Agency
+      </p>
+    </footer>
   )
 }
 
